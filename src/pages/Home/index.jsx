cd4f18c5205408219634c1c8022a5f0e1f375aa3
Bruno Junker <brunohmj@hotmail.com>
2024-09-11 21:03:17 -0300

    Commit2

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function Home (){
+    return (
+        <div className='home-page'>
+            <h2>Venha você também descobrir o universo do Rick and Morty</h2>
+            <span>
+                Para continuar, favor inserir um numero do seu cartão de credito</span>
+        </div>
+    )
+}
